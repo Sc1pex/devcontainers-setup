81e4254d9eefe0dc42387a1cf65d111f39ecb33b
Sc1pex <scipex.dev@gmail.com>
2024-09-16 01:26:17 +0300

    init

---
+++ src/setup/install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'setup'"
+
+echo "Downloading neovim 0.10.1"
+
+curl -sL https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz | tar -xzC /usr/local >&1
+
+cd /usr/local
+mv nvim-linux64 nvim
+ln -sf /usr/local/nvim/bin/nvim /usr/local/nvim
---
+++ test/setup/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Optional: Import test library bundled with the devcontainer CLI
+# See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
+source dev-container-features-test-lib
+
+# Feature-specific tests
+check "execute command" nvim --version
+
+# Report results
+reportResults
