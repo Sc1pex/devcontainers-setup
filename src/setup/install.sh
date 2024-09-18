110a81bddf4190460af6a7cd11ea20c8c13d44e7
Sc1pex <scipex.dev@gmail.com>
2024-09-18 23:16:45 +0300

    add socat

--- src/setup/install.sh
+++ src/setup/install.sh
@@ -6,7 +6,7 @@ echo "Activating feature 'setup'"
 echo "Downloading dependencies"
 apt-get update -y
 apt-get upgrade -y
-apt-get install curl git -y
+apt-get install curl git socat -y
 
 echo "Downloading neovim 0.10.1"
 
